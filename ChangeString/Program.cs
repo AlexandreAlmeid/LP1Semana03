30e7816bfb807c558636d29694ddc88ca91abee1
AlexandreAlmeid <alexandreruizdealmeida@gmail.com>
2024-03-08 19:43:18 +0000

    ChangeString

---
+++ ChangeString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insere uma string: ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("Insere um caracter: ");
+            string read = Console.ReadLine();
+            char chr = char.Parse(read);
+
+            foreach (char c in str)
+            {
+                if (c == chr)
+                {
+                    Console.Write("X");
+                }
+                else
+                {
+                    Console.Write(c);
+                }
+            }
+        }
+    }
+}
