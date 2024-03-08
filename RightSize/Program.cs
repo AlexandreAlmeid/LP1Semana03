bef6fe16aeb583a69678e0a98b24232a429f1660
AlexandreAlmeid <alexandreruizdealmeida@gmail.com>
2024-03-08 18:54:28 +0000

    RigthSize

---
+++ RightSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Argumentos com comprimento superior a 3 caracteres:");
+
+            foreach (string argument in args)
+            {
+                if (argument.Length > 3 && argument.Length <= 6)
+                {
+                    Console.WriteLine(argument);
+                }
+                else if (argument.Length > 6)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
